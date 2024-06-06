20ea000cc12c0c5ed7262e5221e40b2b4400f55a
sukhw <sukhw@LAPTOP-UH4RN7TJ>
2024-06-06 23:59:36 +0530

    Add project files.

---
+++ NunitDemonstration/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnitDemonstration;
+using System;
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int firstAngle = 60;
+        int secondAngle = 60;
+        int thirdAngle = 60;
+        string result = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+        Console.WriteLine(result);
+    }
+}
+
